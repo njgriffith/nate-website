98ee1e61aa2d43ec88d7fd9c45464607f38c7786
njgriffith <njgriffith1@gmail.com>
2024-09-09 00:49:32 -0400

    main page styling change

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -15,69 +15,69 @@ document.addEventListener('DOMContentLoaded', function() {
     const gridContainer = document.getElementById('grid-container');
 
     const items = [
-    { type: 'image', src: '/resources/albums/covers/ants_from_up_there.jpg' },
+    { type: 'image', src: '/resources/albums/covers/ants_from_up_there.jpg', class: 'cover' },
     { type: 'text', content: 'Ants From Up There' },
     { type: 'text', content: 'Black Country New Road' },
-    { type: 'text', content: '10'},
+    { type: 'text', content: '10', class: 'score'},
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/blood_visions.jpg' },
     { type: 'text', content: 'Blood Visions' },
     { type: 'text', content: 'Jay Reatard' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/congratulations.png' },
     { type: 'text', content: 'Congratulations' },
     { type: 'text', content: 'MGMT' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/girl_with_basket_of_fruit.png' },
     { type: 'text', content: 'Girl With Basket of Fruit' },
     { type: 'text', content: 'Xiu Xiu' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/lonesome_crowded_west.jpg' },
     { type: 'text', content: 'The Lonesome Crowded West' },
     { type: 'text', content: 'Modest Mouse' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/skinny_fists.jpg' },
     { type: 'text', content: 'Lift Your Skinny Fists Like Antennas to Heaven' },
     { type: 'text', content: 'Godspeed You! Black Emperor' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/STGSTV.png' },
     { type: 'text', content: 'Spirit They\'re Gone, Spirit They\'ve Vanished' },
     { type: 'text', content: 'Animal Collective' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/to_be_kind.jpg' },
     { type: 'text', content: 'To Be Kind' },
     { type: 'text', content: 'Swans' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/TPAB.jpg' },
     { type: 'text', content: 'To Pimp a Butterfly' },
     { type: 'text', content: 'Kendrick Lamar' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' },
 
     { type: 'image', src: '/resources/albums/covers/TVU&N.jpg' },
     { type: 'text', content: 'The Velvet Underground & Nico' },
     { type: 'text', content: 'The Velvet Underground & Nico' },
-    { type: 'text', content: '10' },
+    { type: 'text', content: '10', class: 'score' },
     { type: 'link', content: 'Full Review', href: '' }
     ];
 
     // Function to create and append grid items based on the type
-    items.forEach(item => {
+    items.forEach((item, index) => {
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     
