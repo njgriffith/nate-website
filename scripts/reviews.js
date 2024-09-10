542b692189cf09f96823d967315996d035cac246
njgriffith <njgriffith1@gmail.com>
2024-09-10 16:18:31 -0400

    minor tweaks

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,16 +1,3 @@
-// function loadText() {
-//     fetch('/resources/albums/reviews/ants_from_up_there.txt')
-//         .then(response => response.text())
-//         .then(data => {
-//             // Insert the text content into the div
-//             document.getElementById('ants_from_up_there').textContent = data;
-//         })
-//         .catch(error => console.error('Error loading text file:', error));
-// }
-
-// // Call the function to load the text
-// loadText();
-
 document.addEventListener('DOMContentLoaded', function() {
     const gridContainer = document.getElementById('grid-container');
 
