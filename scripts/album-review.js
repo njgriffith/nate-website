1504ba57382e8c4749f684620d078258b5c346b7
njgriffith <njgriffith1@gmail.com>
2024-09-19 00:26:04 -0400

    dynamically load scores + more reviews

--- scripts/album-review.js
+++ scripts/album-review.js
@@ -6,28 +6,31 @@ function getUrlParameter(param) {
 const title = getUrlParameter('album');
 const dynamicContentElement = document.getElementById('album-title');
 const cover = document.getElementById('album-cover');
+console.log(title);
 
 
 fetch(`/resources/albums/reviews/${title}.txt`)
     .then(response => response.text())
-    .then(data => {
-        document.getElementById('review').innerHTML = data;
+    .then(textData => {
+        document.getElementById('review').innerHTML = textData;
+        fetch('../resources/albums/album_data.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(jsonData => {
+            document.title = jsonData[`${title}`]['title'];
+            cover.src = `/resources/albums/covers/${title}.jpg`;
+            dynamicContentElement.textContent = jsonData[`${title}`]['title'];
+            document.getElementById('review').innerHTML += '<br>' + jsonData[`${title}`]['score'];
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
     })
     .catch(error => console.error('Error loading text file:', error));
 
 
-fetch('../resources/albums/album_data.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        document.title = data[`${title}`]['title'];
-        cover.src = `/resources/albums/covers/${title}.jpg`;
-        dynamicContentElement.textContent = data[`${title}`]['title'];
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+
