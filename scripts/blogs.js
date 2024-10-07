008e535f94252d31bf5693e42648b5580a3a4d44
njgriffith <njgriffith1@gmail.com>
2024-10-07 17:04:31 -0400

    puzzle levels 1-9 live, some reviews

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,10 +1,9 @@
 function getRandomReview(){
     fetch('../resources/albums/album_data.json')
-        .then(response => response.json())  // Fetch and parse the JSON file
+        .then(response => response.json())
         .then(data => {
             let index = Math.trunc(Math.random() * Object.keys(data).length);
             const keys = Object.keys(data);
             window.location.href=`/templates/album-review.html?album=${keys[index]}`;
         });
 }
-// getRandomReview();
--- scripts/reviews.js
+++ scripts/reviews.js
@@ -184,4 +184,12 @@ function sortAndReloadContent(columnName, element) {
 
 sortAndReloadContent('reviewed', null);
 
-
+function getRandomReview(){
+    fetch('../resources/albums/album_data.json')
+        .then(response => response.json())
+        .then(data => {
+            let index = Math.trunc(Math.random() * Object.keys(data).length);
+            const keys = Object.keys(data);
+            window.location.href=`/templates/album-review.html?album=${keys[index]}`;
+        });
+}
