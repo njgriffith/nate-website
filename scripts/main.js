15baf0d246f00aef1a00724d4a6d200e7f2d1175
njgriffith <njgriffith1@gmail.com>
2024-11-22 18:00:25 -0500

    y2k home theme initial push

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,164 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+// open and close start menu
+document.getElementById('start-button').addEventListener('click', function () {
+  const startMenu = document.getElementById('start-menu');
+  startMenu.classList.toggle('hidden');
+});
 
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+document.addEventListener('click', function (event) {
+  const startMenu = document.getElementById('start-menu');
+  const startButton = document.getElementById('start-button');
+  if (!startMenu.contains(event.target) && event.target !== startButton) {
+    startMenu.classList.add('hidden');
+  }
+});
+// date/time in bottom right
+function updateDateTime() {
+  const datetimeElement = document.getElementById("datetime");
+  const now = new Date();
+  const options = { hour: '2-digit', minute: '2-digit', weekday: 'short', month: 'short', day: 'numeric' };
+  const formattedDate = now.toLocaleDateString(undefined, options).replace(',', '').replace(', ', '<br>');
+
+  datetimeElement.innerHTML = formattedDate;
 }
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+updateDateTime();
+setInterval(updateDateTime, 60000);
 
-resizeCanvas();
-window.addEventListener('resize', resizeCanvas);
 
-class Rain {
-    constructor() {
-        this.x = getRandomInt(canvas.width);
-        this.y = getRandomInt(canvas.height);
-        this.height = getRandomInt(60) + 10;
-        this.width = getRandomInt(7) + 2;
-        this.speed = getRandomInt(7) + 4;
-    }
-}
+// add hover event listeners for folder icons
+const mediaLibrary = document.getElementById('media-library');
+const folderImages = mediaLibrary.querySelectorAll('.folder-img');
+const contentContainer = document.getElementById('folder-content');
 
-const drops = [];
-for (let i = 0; i < 150; i++) {
-    drops.push(new Rain());
-}
-function draw() {
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+folderImages.forEach(img => {
+  const originalSrc = img.src;
+  const hoverSrc = '/resources/msft/folder-open.png';
 
-    drops.forEach(function (drop) {
-        ctx.fillStyle = '#8849cc';
-        ctx.fillRect(drop.x, drop.y, drop.width, drop.height);
-        drop.y += drop.speed;
+  img.addEventListener('mouseenter', function () {
+    img.src = hoverSrc;
+  });
 
-        // loop to top
-        if (drop.y > canvas.height) {
-            drop.y = -drop.height;
-        }
+  img.addEventListener('mouseleave', function () {
+    img.src = originalSrc;
+  });
+});
+
+// load folder's content on click
+async function loadFolderContents(folderName) {
+  try {
+    const response = await fetch('/resources/files.json');
+    const data = await response.json();
+    const folderContent = data[folderName];
+    contentContainer.innerHTML = '';
+
+    document.getElementById('back-button').style.display = 'block';
+    document.getElementById('main-content').style.display = 'none';
+
+    folderContent.forEach(item => {
+      const itemDiv = document.createElement('div');
+      itemDiv.classList.add('file-item');
+
+      const title = document.createElement('div');
+      const image = document.createElement('img');
+
+      title.textContent = item;
+      image.src = `/resources/msft/${folderName}-file.png`;
+
+      itemDiv.appendChild(image);
+      itemDiv.appendChild(title);
+      itemDiv.style.cursor = "pointer";
+      itemDiv.addEventListener('click', () => playMedia(item, folderName));
+
+      contentContainer.appendChild(itemDiv);
+      contentContainer.classList.add('file-system')
     });
-
-    // loop
-    requestAnimationFrame(draw);
+  } catch (error) {
+    console.log('Folder not found ', error);
+  }
 }
-draw();
+// back button
+function goBack() {
+  document.getElementById('folder-content').innerHTML = '';
+  document.getElementById('back-button').style.display = 'none';
+  document.getElementById('main-content').style.display = 'flex';
+  document.getElementById('audio-controller').style.display = "none";
+  document.getElementById('image-viewer').style.display = "none";
+  const videoPlayer = document.getElementById('video-player');
+  videoPlayer.style.display = 'none';
+  videoPlayer.pause();
+
+}
+function handleFolderClick(event) {
+  const folderName = event.target.closest('.folder').dataset.folder;
+  if (folderName) {
+    loadFolderContents(folderName);
+  }
+}
+
+document.getElementById('music-folder').addEventListener('click', handleFolderClick);
+document.getElementById('videos-folder').addEventListener('click', handleFolderClick);
+document.getElementById('pictures-folder').addEventListener('click', handleFolderClick);
+
+const imageViewer = document.getElementById('image-viewer');
+// play selected media
+function playMedia(fileName, folderName) {
+  const audioPlayer = document.getElementById('audio-player');
+  const videoPlayer = document.getElementById('video-player');
+  const controller = document.getElementById('audio-controller');
+
+  if (folderName === "music"){
+    const mediaPath = `/resources/media/${fileName}.mp3`;
+    audioPlayer.src = mediaPath;
+    controller.style.display = 'block';
+    audioPlayer.play();
+  }
+  else if (folderName === "videos"){
+    const mediaPath = `/resources/media/${fileName}.mp4`;
+    videoPlayer.style.display = 'block';
+  }
+  else if (folderName === "pictures"){
+    imageViewer.style.display = "block";
+    const mediaPath = `/resources/media/${fileName}.png`;
+    const image = document.createElement('img');
+    image.classList.add('library-image')
+    image.src = mediaPath;
+    imageViewer.innerHTML = "";
+    imageViewer.appendChild(image);
+  }
+}
+
+document.getElementById('volume').addEventListener('input', function (event) {
+  const audioPlayer = document.getElementById('audio-player');
+  audioPlayer.volume = event.target.value / 10;
+});
+
+// open/close media library
+function openMediaLibrary(){
+  document.getElementById('media-library').style.display = 'block';
+}
+
+function closeMediaLibrary(){
+  document.getElementById('media-library').style.display = 'none';
+}
+
+// drag and drop media library
+const titleBar = mediaLibrary.querySelector(".title-bar");
+let isDragging = false;
+
+// pick up
+titleBar.addEventListener("mousedown", (event) => {
+    isDragging = true;
+});
+
+// drag
+document.addEventListener("mousemove", (event) => {
+  // console.log(event.clientX, event.clientY);
+    if (isDragging) {
+        mediaLibrary.style.left = `${event.clientX}px`;
+        mediaLibrary.style.top = `${event.clientY + (mediaLibrary.clientHeight/2) - 5}px`;
+    }
+});
+
+// drop
+document.addEventListener("mouseup", () => {
+    isDragging = false;
+});
