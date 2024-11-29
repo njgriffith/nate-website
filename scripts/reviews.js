e8cd5a74dc4f84f4585629f62af57cdc29e7794a
njgriffith <njgriffith1@gmail.com>
2024-11-29 17:40:53 -0500

    updated reviews to y2k style

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,28 +1,32 @@
+const reviewTable = document.getElementById('review-table');
 window.addEventListener('beforeunload', function () {
-    sessionStorage.setItem('scrollPosition', window.scrollY);
+    const scrollableDiv = document.getElementById('scroll-div');
+    if (scrollableDiv) {
+        sessionStorage.setItem('scrollPosition', scrollableDiv.scrollTop);
+    }
 });
+
 window.addEventListener('load', function () {
+    const scrollableDiv = document.getElementById('scroll-div');
     const savedScrollPosition = sessionStorage.getItem('scrollPosition');
 
-    if (savedScrollPosition) {
+    if (scrollableDiv && savedScrollPosition) {
         setTimeout(() => {
-            window.scrollTo(0, parseInt(savedScrollPosition));
+            scrollableDiv.scrollTop = parseInt(savedScrollPosition);
         }, 100);
     }
 });
 
 function sortData(columnName, sortDirection) {
-    const gridContainer = document.getElementById('grid-container');
     fetch('../resources/albums/album_data.json')
         .then(response => response.json())
         .then(data => {
             var sort = columnName;
-
             const entries = Object.entries(data);
 
             if (sort === "title") {
                 entries.sort((a, b) => {
-                    if (sortDirection === 'desc') {
+                    if (sortDirection) {
                         return a[1].title.localeCompare(b[1].title);
                     }
                     return b[1].title.localeCompare(a[1].title);
@@ -30,7 +34,7 @@ function sortData(columnName, sortDirection) {
             }
             else if (sort === "artist") {
                 entries.sort((a, b) => {
-                    if (sortDirection === 'desc') {
+                    if (sortDirection) {
                         return a[1].artist.localeCompare(b[1].artist);
                     }
                     return b[1].artist.localeCompare(a[1].artist);
@@ -38,7 +42,7 @@ function sortData(columnName, sortDirection) {
             }
             else if (sort === "score") {
                 entries.sort((a, b) => {
-                    if (sortDirection === 'desc') {
+                    if (sortDirection) {
                         if (a[1].score === 10 && b[1].score === 10) {
                             return a[1].rank - b[1].rank;
                         }
@@ -58,7 +62,7 @@ function sortData(columnName, sortDirection) {
                 entries.sort((a, b) => {
                     aValue = new Date(a[1].released);
                     bValue = new Date(b[1].released);
-                    if (sortDirection === 'desc') {
+                    if (sortDirection) {
                         return bValue - aValue;
                     }
                     return aValue - bValue;
@@ -68,7 +72,7 @@ function sortData(columnName, sortDirection) {
                 entries.sort((a, b) => {
                     aValue = new Date(a[1].reviewed);
                     bValue = new Date(b[1].reviewed);
-                    if (sortDirection === 'desc') {
+                    if (sortDirection) {
                         return bValue - aValue;
                     }
                     return aValue - bValue;
@@ -78,76 +82,44 @@ function sortData(columnName, sortDirection) {
 
             for (let key in sortedData) {
                 if (sortedData.hasOwnProperty(key)) {
-                    // dynamically create image with link
-                    const coverItem = document.createElement('div');
-                    coverItem.classList.add('grid-item');
-
+                    // create image
+                    const coverItem = document.createElement('td');
                     const img = document.createElement('img');
                     img.src = `${sortedData[key].cover}`
-                    img.classList.add('cover');
-                    const imageLink = document.createElement('a');
-                    imageLink.href = `/templates/album-review.html?album=${key}`;
-                    imageLink.classList.add('image-link');
-
-                    imageLink.appendChild(img);
-                    coverItem.appendChild(imageLink);
-                    gridContainer.appendChild(coverItem);
+                    coverItem.appendChild(img);
 
                     // create title
-                    const titleItem = document.createElement('div');
-                    titleItem.classList.add('grid-item');
-                    const title = document.createElement('p');
-                    title.textContent = `${sortedData[key].title}`;
-                    titleItem.appendChild(title);
+                    const titleItem = document.createElement('td');
+                    titleItem.textContent = `${sortedData[key].title}`;
 
                     // create artist
-                    const artistItem = document.createElement('div');
-                    artistItem.classList.add('grid-item');
-                    const artist = document.createElement('p');
-                    artist.textContent = `${sortedData[key].artist}`;
-                    artistItem.appendChild(artist);
+                    const artistItem = document.createElement('td');
+                    artistItem.textContent = `${sortedData[key].artist}`;
 
                     // create score
-                    const scoreItem = document.createElement('div');
-                    scoreItem.classList.add('grid-item');
-                    const score = document.createElement('p');
-                    score.textContent = `${sortedData[key].score}`;
-                    scoreItem.appendChild(score);
+                    const scoreItem = document.createElement('td');
+                    scoreItem.textContent = `${sortedData[key].score}`;
 
                     // create releaseDate
-                    const releaseItem = document.createElement('div');
-                    releaseItem.classList.add('grid-item');
-                    const release = document.createElement('p');
-                    release.textContent = `${sortedData[key].released}`;
-                    releaseItem.appendChild(release);
+                    const releaseItem = document.createElement('td');
+                    releaseItem.textContent = `${sortedData[key].released}`;
 
                     // create reviewDate
-                    const reviewItem = document.createElement('div');
-                    reviewItem.classList.add('grid-item');
-                    const reviewDate = document.createElement('p');
-                    reviewDate.textContent = `${sortedData[key].reviewed}`;
-                    reviewItem.appendChild(reviewDate);
-
-                    // create review link
-                    const linkItem = document.createElement('div');
-                    linkItem.classList.add('grid-item');
-                    const link = document.createElement('a');
-                    link.classList.add('text-link');
-                    link.href = `/templates/album-review.html?album=${key}`;
-                    link.textContent = "Full Review";
-                    linkItem.appendChild(link);
+                    const reviewItem = document.createElement('td');
+                    reviewItem.textContent = `${sortedData[key].reviewed}`;
 
                     // append items to row, then append row to page
-                    const row = document.createElement('div');
-                    row.classList.add('grid-row');
+                    const row = document.createElement('tr');
                     row.appendChild(coverItem);
                     row.appendChild(titleItem);
                     row.appendChild(artistItem);
                     row.appendChild(scoreItem);
                     row.appendChild(releaseItem);
                     row.appendChild(reviewItem);
+                    row.setAttribute("onclick", "toReview(this)");
+                    row.id=`${key}`;
 
-                    gridContainer.appendChild(row);
+                    reviewTable.appendChild(row);
                 }
             }
         })
@@ -156,37 +128,27 @@ function sortData(columnName, sortDirection) {
         });
 };
 
-let sortDirection = 'desc'; // Default sort direction
+let sortDirection = true;
 function sortAndReloadContent(columnName, element) {
     if (element === null) {
-        const arrow = document.getElementById(columnName.toLowerCase() + '-arrow');
-        arrow.classList.add('sort-desc');
+        const selected = document.getElementById('reviewed');
+        selected.classList.add('selected-sort');
         sortData(columnName, sortDirection);
         return;
     }
 
-    var sortName = element.childNodes[0].nodeValue.trim().toLowerCase();
-    const arrow = element.querySelector('span');
-
-    if (arrow.classList.contains('sort-desc')) {
-        sortDirection = 'asc';
-    } else {
-        sortDirection = 'desc';
-    }
-    // Remove sort classes from all headers
-    document.querySelectorAll('.sort-arrow').forEach(sortArrow => {
-        sortArrow.classList.remove('sort-asc', 'sort-desc');
-    });
-    arrow.classList.add(sortDirection === 'asc' ? 'sort-asc' : 'sort-desc');
-    if (sortDirection === 'asc') {
-        arrow.innerText = '▲';
+    if (element.classList.contains('selected-sort')) {
+        sortDirection = !sortDirection;
     }
     else {
-        arrow.innerText = '▼';
+        sortDirection = true;
+        document.querySelectorAll('th').forEach(colTitle => {
+            colTitle.classList.remove('selected-sort');
+        });
+        element.classList.add('selected-sort');
     }
 
-
-    const divs = document.querySelectorAll('.grid-row');
+    const divs = document.querySelectorAll('tbody>tr');
     divs.forEach(div => {
         div.remove();
     });
@@ -203,5 +165,8 @@ function getRandomReview() {
             const keys = Object.keys(data);
             window.location.href = `/templates/album-review.html?album=${keys[index]}`;
         });
+}
+function toReview(element){
+    window.location.href = `/templates/album-review.html?album=${element.id}`;
 }
 
